cac7999e11bcb850421a679b4e7037d06521e33a
samspeactre <samspectre1@gmail.com>
2024-08-21 16:59:24 -0700

    complete desktop screen

--- src/app/components/services/page-banner.service.ts
+++ src/app/components/services/page-banner.service.ts
@@ -7,27 +7,31 @@ import { filter } from 'rxjs';
 })
 export class PageBannerService {
 
-  private titles: { [key: string]: string } = {
-    '/': 'Home',
-    '/pricing': 'Pricing',
-    '/about': 'About Us',
-    '/terms-conditions': 'Terms & Conditions',
-    '/privacy-policy': 'Privacy Policy',
-    '/contact': 'Contact Us',
-    '/employers': 'Employers',
-    '/employer-details': 'Employer Details',
-    '/candidates': 'Candidates',
-    '/candidates-details': 'Candidates Details',
+  private titles: { [key: string]: { h1: string, li: string } } = {
+    '/pricing': { h1: 'Our Pricing', li: 'Pricing' },
+    '/about': { h1: 'About Us', li: 'About Us' },
+    '/terms-conditions': { h1: 'Terms & Conditions', li: 'Terms & Conditions' },
+    '/privacy-policy': { h1: 'Privacy Policy', li: 'Privacy Policy' },
+    '/contact': { h1: 'Contact Us', li: 'Contact Us' },
+    '/employers': { h1: 'Employers', li: 'Employers' },
+    '/employer-details': { h1: 'Employer Details', li: 'Employer Details' },
+    '/candidates': { h1: 'Candidates', li: 'Candidates' },
+    '/candidate-details': { h1: 'Candidate Details', li: 'Candidate Details' },
+    '/jobs-listing': { h1: 'There are 58,612 jobs for you', li: 'Jobs Listing' },
+    '/jobs-grid': { h1: 'There are 58,612 jobs for you', li: 'Jobs Grid' },
     // Add more routes and titles as needed
   };
 
-  public currentTitle: string = '';
+  public currentH1Title: string = '';
+  public currentLiTitle: string = '';
 
   constructor(private router: Router) {
     this.router.events
       .pipe(filter((event: any) => event instanceof NavigationEnd))
       .subscribe((event: any) => {
-        this.currentTitle = this.titles[event.url] || 'Page';
+        const titles = this.titles[event.url] || { h1: 'Page', li: 'Page' };
+        this.currentH1Title = titles.h1;
+        this.currentLiTitle = titles.li;
       });
   }
 }
