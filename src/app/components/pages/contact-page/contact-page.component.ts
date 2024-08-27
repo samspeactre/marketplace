fd6f3b1d32589e2a3b55b87c86dc18df9790d67a
noahconner <noahconner1512@gmail.com>
2024-08-27 11:20:09 +0500

    chnges

--- src/app/components/pages/contact-page/contact-page.component.ts
+++ src/app/components/pages/contact-page/contact-page.component.ts
@@ -1,43 +1,45 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import {FormGroup, FormControl,Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-contact-page',
-    templateUrl: './contact-page.component.html',
-    styleUrls: ['./contact-page.component.scss']
+  selector: 'app-contact-page',
+  templateUrl: './contact-page.component.html',
+  styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent {
 
-    title = 'Contact Us - Jove';
-    // Name = new FormControl('');
+  title = 'Contact Us - Jove';
+  submit:boolean = false;
+  // Name = new FormControl('');
 
-    ContactPageForm= new FormGroup({
-        Name: new FormControl(null,[Validators.required,Validators.minLength(3)]),
-        Email: new FormControl(null,[Validators.required,Validators.email]),
-        phone: new FormControl(null,[Validators.required,Validators.pattern('^[0-9]*$')]),
-        Subject: new FormControl(null,[Validators.required]),
-        yourMessage: new FormControl(null,[Validators.required,Validators.minLength(10)]),
-    })
+  ContactPageForm = new FormGroup({
+    Name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    Email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [Validators.required]),
+    Subject: new FormControl(null, [Validators.required]),
+    yourMessage: new FormControl(null, [Validators.required, Validators.minLength(10)]),
+  })
 
-    
-//   onSubmit() {
-//     // TODO: Use EventEmitter with form value
-//     console.warn(this.ContactPageForm.value);
-//   }
 
-onSubmit() {
+  //   onSubmit() {
+  //     // TODO: Use EventEmitter with form value
+  //     console.warn(this.ContactPageForm.value);
+  //   }
+
+  onSubmit() {
+    this.submit = true
     if (this.ContactPageForm.valid) {
-      console.warn(this.ContactPageForm.value);
+      console.log(this.ContactPageForm.value);
     } else {
       console.log('Form is invalid');
     }
   }
- 
-    constructor(private titleService:Title) {}
-    
-    ngOnInit() {
-        this.titleService.setTitle(this.title);
-    }
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 
 }
